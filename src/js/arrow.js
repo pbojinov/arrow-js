157bb8d0ef83e8942699ef3a4eaaa2ac1ae3988c
pbojinov <petarbojinov@gmail.com>
2013-10-14 18:01:09 -0700

    got ie8 rotations to work, see js access to ie8 css rotate in arrow-ie8.html

--- src/js/arrow.js
+++ src/js/arrow.js
@@ -26,11 +26,32 @@ window.Arrow = (function(window, document, undefined) {
             M = ua.match(/(opera|chrome|safari|firefox|msie|trident)\/?\s*([\d\.]+)/i) || [];
         M = M[2] ? [M[1], M[2]] : [N, navigator.appVersion, '-?'];
         if (M && (tem = ua.match(/version\/([\.\d]+)/i)) != null) M[2] = tem[1];
-        browser = M[0];
+        browser = M[0].toLowerCase();
         browserVersion = M[1];
     })();
 
+    var _ie8ArrowStyle = function() {
+
+    };
+
     var _caculateArrowPosition = function() {
+
+        if (browser === 'msie') {
+            if (browserVersion === '8.0') {
+
+            } else if ((browserVersion === '9.0') || (browserVersion === '10.0')) {
+
+            } else {
+                //we just throw it the full feature set
+            }
+
+        } else if (browser === 'chrome') {
+
+        } else if (browser === 'firefox') {
+
+        } else {
+            //we just throw it the full feature set
+        }
 
     };
 
