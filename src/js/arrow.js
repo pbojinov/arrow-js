bd37068544110c6af89a1f8e8ef4025128fd7a76
e_palm <epalmans@gmail.com>
2018-12-11 12:11:57 +0100

    image url in variabele

--- src/js/arrow.js
+++ src/js/arrow.js
@@ -15,7 +15,8 @@ window.Arrow = (function (window, document, undefined) {
         browser = '',
         browserVersion = 0,
         visibleHeight = 0,
-        visibleWidth = 0;
+        visibleWidth = 0,
+        imageUrl = 'https://i.imgur.com/aMwoyfN.png';
 
     /**
      * Other available arrows to use. Planning on adding more colors
@@ -128,7 +129,7 @@ window.Arrow = (function (window, document, undefined) {
         node.style.height = '309px';
         node.style.width = '186px';
         node.style.opacity = 0;
-        node.style.backgroundImage = 'url(https://i.imgur.com/aMwoyfN.png)';
+        node.style.backgroundImage = 'url(' + imageUrl + ')';
         node.style.backgroundRepeat = 'no-repeat';
         node.style.backgroundPositionX = '0';
         node.style.backgroundPositionY = '0';
@@ -147,7 +148,7 @@ window.Arrow = (function (window, document, undefined) {
 
         // Only one filter style can exist so we concatenate them to one line
         var opacity = 'progid:DXImageTransform.Microsoft.Alpha(opacity=0) ',
-            imgSrc = 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="https://i.imgur.com/aMwoyfN.png", sizingMethod="scale") ',
+            imgSrc = 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + imageUrl + '", sizingMethod="scale") ',
             rotation = 'progid:DXImageTransform.Microsoft.Matrix(M11=1, M12=1.2246063538223773e-16, M21=-1.2246063538223773e-16, M22=-1, SizingMethod="auto expand") ';
 
         node.style.filter = opacity + imgSrc + rotation;
